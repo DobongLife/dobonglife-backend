fb09d89643fa4702e7a7f37368f0ba61e9ac4203
Shinjongyun <tigerjong123@gmail.com>
2025-10-24 14:38:32 +0900

    Initial commit

---
+++ src/test/java/com/umust/dobonglife/DobonglifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umust.dobonglife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobonglifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
