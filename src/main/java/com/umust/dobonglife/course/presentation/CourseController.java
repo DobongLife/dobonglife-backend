3b762850b5f393a2584eb1d5b6b3b3fd1a9dbfc7
gongPyeon <pyeonkang@naver.com>
2025-11-07 13:42:25 +0900

    feat: 코스 상세 조회 기능 구현 (#4)

--- src/main/java/com/umust/dobonglife/course/presentation/CourseController.java
+++ src/main/java/com/umust/dobonglife/course/presentation/CourseController.java
@@ -1,14 +1,12 @@
 package com.umust.dobonglife.course.presentation;
 
+import com.umust.dobonglife.course.presentation.dto.response.CourseDetailResponse;
 import com.umust.dobonglife.course.presentation.dto.response.CourseSummaryResponse;
 import com.umust.dobonglife.course.service.CourseService;
 import com.umust.dobonglife.global.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +21,12 @@ public class CourseController {
                                                                 @RequestParam(defaultValue = "2") int size) {
         Page<CourseSummaryResponse> responses = courseService.getCourses(page, size);
         return BaseResponse.ok(responses);
+    }
+
+    // 코스 상세보기 조회
+    @GetMapping("/{courseId}")
+    public BaseResponse<CourseDetailResponse> getCourse(@PathVariable("courseId") Integer courseId){
+        CourseDetailResponse response = courseService.getCourse(courseId);
+        return BaseResponse.ok(response);
     }
 }
--- src/main/java/com/umust/dobonglife/course/service/CourseService.java
+++ src/main/java/com/umust/dobonglife/course/service/CourseService.java
@@ -1,8 +1,13 @@
 package com.umust.dobonglife.course.service;
 
 import com.umust.dobonglife.course.domain.entity.Course;
+import com.umust.dobonglife.course.domain.entity.CoursePlans;
+import com.umust.dobonglife.course.domain.repository.CoursePlansRepository;
+import com.umust.dobonglife.course.exception.CourseException;
+import com.umust.dobonglife.course.presentation.dto.response.CourseDetailResponse;
 import com.umust.dobonglife.course.presentation.dto.response.CourseSummaryResponse;
 import com.umust.dobonglife.course.domain.repository.CourseRepository;
+import com.umust.dobonglife.global.common.response.ErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,14 +15,25 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CourseService {
     private final CourseRepository courseRepository;
+    private final CoursePlansRepository coursePlansRepository;
     @Transactional
     public Page<CourseSummaryResponse> getCourses(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<Course> courses = courseRepository.findAllRandomOrder(pageable);
         return courses.map(CourseSummaryResponse::from);
+    }
+
+    public CourseDetailResponse getCourse(Integer courseId) {
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new CourseException(ErrorCode.INVALID_COURSE_ID));
+
+        List<CoursePlans> plans = coursePlansRepository.findByCourseIdOrderByDateTime(courseId);
+        return CourseDetailResponse.from(course, plans);
     }
 }
