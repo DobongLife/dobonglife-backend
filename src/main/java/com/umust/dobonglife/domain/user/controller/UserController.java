e595b742ffb7b376c111e3f4e7ea3de2ecce3a23
Shinjongyun <tigerjong123@gmail.com>
2025-11-14 12:20:27 +0900

    [Feat] #3 UserController에 회원 가입 & 탈퇴 추가

---
+++ src/main/java/com/umust/dobonglife/domain/user/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.umust.dobonglife.domain.user.controller;
+
+import com.umust.dobonglife.global.common.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public BaseResponse<Void> signUp(@RequestBody SignupRequest request) {
+        userService.signUp(request);
+        return BaseResponse.ok(null);
+    }
+
+    @PostMapping("/delete/account")
+    public BaseResponse<Void> deleteAccount(HttpServletRequest request,
+                                            @CurrentUserId Long userId) {
+        userService.deleteAccount(request, userId);
+        return BaseResponse.ok(null);
+    }
+}
+
