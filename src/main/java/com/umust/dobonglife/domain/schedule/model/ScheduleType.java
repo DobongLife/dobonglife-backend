fab836978fa4ce80666a47ed27981957d938a4ff
Shinjongyun <tigerjong123@gmail.com>
2025-11-21 16:54:22 +0900

    [Feat] #16 ScheduleType enum

---
+++ src/main/java/com/umust/dobonglife/domain/schedule/model/ScheduleType.java
@@ -0,0 +1,20 @@
+package com.umust.dobonglife.domain.schedule.model;
+
+public enum ScheduleType {
+    PERSONAL,
+    EVENT,
+    ACTIVITY;
+
+    public static ScheduleType toEnum(String value) {
+        for (ScheduleType scheduleType : ScheduleType.values()) {
+            if (scheduleType.name().equalsIgnoreCase(value)) {
+                return scheduleType;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 enum입니다: " + value);
+    }
+
+    public String toValue() {
+        return name();
+    }
+}
