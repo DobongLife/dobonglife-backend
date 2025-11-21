19a935e0f50fc67b3305c727a4701ff27e9eb721
Shinjongyun <tigerjong123@gmail.com>
2025-11-21 15:00:50 +0900

    [Feat] #16 CoursePlace 엔티티

---
+++ src/main/java/com/umust/dobonglife/domain/course/model/CoursePlace.java
@@ -0,0 +1,38 @@
+package com.umust.dobonglife.domain.course.model;
+
+import com.umust.dobonglife.domain.place.model.Place;
+import com.umust.dobonglife.domain.review.model.Template;
+import com.umust.dobonglife.global.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "course_places")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CoursePlace extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_place_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id", nullable = true)
+    private Place place;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id", nullable = true)
+    private Course course;
+
+    @ElementCollection(targetClass = Theme.class, fetch = FetchType.LAZY)
+    @CollectionTable(name = "themes", joinColumns = @JoinColumn(name = "course_place_id"))
+    @Enumerated(EnumType.STRING)
+    private List<Theme> themes = new ArrayList<>();
+}
