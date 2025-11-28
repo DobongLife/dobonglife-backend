034968bc6496df4c365ecde9c330d11bb6d04c56
Shinjongyun <tigerjong123@gmail.com>
2025-11-28 14:28:26 +0900

    [Feat] #16 DataInitializer

--- src/main/java/com/umust/dobonglife/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/umust/dobonglife/domain/schedule/service/ScheduleService.java
@@ -1,9 +1,14 @@
 package com.umust.dobonglife.domain.schedule.service;
 
+import com.umust.dobonglife.domain.place.model.Place;
 import com.umust.dobonglife.domain.schedule.dto.request.ScheduleRegisterRequest;
 import com.umust.dobonglife.domain.schedule.model.Schedule;
 import com.umust.dobonglife.domain.schedule.model.ScheduleType;
 import com.umust.dobonglife.domain.schedule.respository.ScheduleRepository;
+import com.umust.dobonglife.domain.user.model.User;
+import com.umust.dobonglife.domain.user.repository.UserRepository;
+import com.umust.dobonglife.global.common.exception.BusinessException;
+import com.umust.dobonglife.global.common.response.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,9 +20,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public void registerSchedule(ScheduleRegisterRequest request, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
         Schedule schedule = Schedule.builder()
                 .title(request.getTitle())
                 .startTime(request.getStartTime())
