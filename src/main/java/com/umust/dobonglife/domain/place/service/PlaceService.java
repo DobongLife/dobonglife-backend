abf9c2ff29ff1a4baff14c0a83cb051dd31d0123
Shinjongyun <tigerjong123@gmail.com>
2025-12-05 14:58:23 +0900

    [Feat] #16 PlaceLike

--- src/main/java/com/umust/dobonglife/domain/place/service/PlaceService.java
+++ src/main/java/com/umust/dobonglife/domain/place/service/PlaceService.java
@@ -10,7 +10,12 @@ import com.umust.dobonglife.domain.place.controller.dto.response.PlaceResponseLi
 import com.umust.dobonglife.domain.place.domain.constant.Amenity;
 import com.umust.dobonglife.domain.place.domain.entity.CoursePlace;
 import com.umust.dobonglife.domain.place.domain.entity.Place;
+import com.umust.dobonglife.domain.place.domain.entity.PlaceLike;
 import com.umust.dobonglife.domain.place.domain.repository.PlaceRepository;
+import com.umust.dobonglife.domain.user.domain.entity.User;
+import com.umust.dobonglife.domain.user.domain.repository.UserRepository;
+import com.umust.dobonglife.global.common.exception.BusinessException;
+import com.umust.dobonglife.global.common.response.ErrorCode;
 import com.umust.dobonglife.global.common.s3.S3Utils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -27,6 +33,7 @@ public class PlaceService {
 
     private final PlaceRepository placeRepository;
     private final CoursePlaceRepository coursePlaceRepository;
+    private final UserRepository userRepository;
     private final S3Utils s3Utils;
 
     @Transactional
@@ -65,5 +72,37 @@ public class PlaceService {
 
         return PlaceResponseList.from(responses);
     }
+
+    @Transactional
+    public boolean toggleLikes(Long userId, Long placeId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+        Place place = placeRepository.findById(placeId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.PLACE_NOT_FOUND));
+
+        Optional<PlaceLike> articleLikesOptional = articleLikesRepository.findByMemberIdAndArticleId(member.getId(), article.getId());
+
+        if(articleLikesOptional.isPresent()) {
+            PlaceLike articleLikes = articleLikesOptional.get();
+
+            if(articleLikes.getStatus() == Status.DELETED) {
+                articleLikes.restore();
+                return true;
+            }
+            else {
+                articleLikes.softDelete();
+                return false;
+            }
+        }
+
+        PlaceLike articleLikes = ArticleLikes.builder()
+                .member(member)
+                .article(article)
+                .build();
+        articleLikesRepository.save(articleLikes);
+        return true;
+    }
+
 
 }
