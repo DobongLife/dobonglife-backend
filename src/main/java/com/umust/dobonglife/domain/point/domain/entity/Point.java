935dcbd79e95839fd231f4e84234a6c0ebdf1340
Shinjongyun <tigerjong123@gmail.com>
2025-12-12 14:22:33 +0900

    [Feat] #26 Point 엔티티

---
+++ src/main/java/com/umust/dobonglife/domain/point/domain/entity/Point.java
@@ -0,0 +1,35 @@
+package com.umust.dobonglife.domain.point.domain.entity;
+
+import com.umust.dobonglife.domain.user.domain.entity.User;
+import com.umust.dobonglife.global.common.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "points")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Point extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "point_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String amount;
+
+    @Column(name = "reason", nullable = false)
+    private String reason;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "point_type", nullable = true)
+    private PointType pointType;
+}
---
+++ src/main/java/com/umust/dobonglife/domain/point/service/PointService.java
@@ -0,0 +1,36 @@
+package com.umust.dobonglife.domain.point.service;
+
+import com.umust.dobonglife.domain.point.domain.entity.Point;
+import com.umust.dobonglife.domain.user.domain.entity.User;
+import com.umust.dobonglife.domain.user.domain.repository.UserRepository;
+import com.umust.dobonglife.global.common.exception.BusinessException;
+import com.umust.dobonglife.global.common.response.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 포인트 계산 결과 필드를 User 엔티티에 두는 이유
+ * 비록 포인트가 갱신될때마다 필드로 갱신해줘야 되서 쿼리가 1개 늘어나긴 하지만,
+ * 대부분의 서비스는 쓰기보다 조회의 경우가 더 많다.
+ * 따라서 포인트 내역이 매우 많을 때는 sum으로 집계하는 것보다
+ * User 엔티티에서 그냥 칼럼 하나로 조회하는 게 더 효율적이다.
+ **/
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PointService {
+
+    private final UserRepository userRepository;
+
+    public PointListResponse getMyPoint (Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        List<Point>
+
+
+    }
+}
