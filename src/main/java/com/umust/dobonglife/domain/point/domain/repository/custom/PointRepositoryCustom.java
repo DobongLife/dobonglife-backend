7eaf72e6bb47a5d4450547151319e4174184130f
Shinjongyun <tigerjong123@gmail.com>
2025-12-12 15:41:41 +0900

    [Refact] #26 findMYPoint Cursor방식

--- src/main/java/com/umust/dobonglife/domain/point/domain/repository/custom/PointRepositoryCustom.java
+++ src/main/java/com/umust/dobonglife/domain/point/domain/repository/custom/PointRepositoryCustom.java
@@ -1,9 +1,10 @@
 package com.umust.dobonglife.domain.point.domain.repository.custom;
 
 import com.umust.dobonglife.domain.point.domain.entity.Point;
+import com.umust.dobonglife.global.common.response.slice.Cursor;
 
 import java.util.List;
 
 public interface PointRepositoryCustom {
-    List<Point> findMyPoint(Long userId);
+    List<Point> findMyPoint(Long userId, int size, Cursor cursor);
 }
--- src/main/java/com/umust/dobonglife/domain/point/infrastructure/PointRepositoryImpl.java
+++ src/main/java/com/umust/dobonglife/domain/point/infrastructure/PointRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.umust.dobonglife.domain.point.infrastructure;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.umust.dobonglife.domain.point.domain.entity.Point;
 import com.umust.dobonglife.domain.point.domain.repository.custom.PointRepositoryCustom;
+import com.umust.dobonglife.global.common.response.slice.Cursor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -17,14 +18,21 @@ public class PointRepositoryImpl implements PointRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<Point> findMyPoint(Long userId) {
+    public List<Point> findMyPoint(Long userId, int size, Cursor cursor) {
 
         return queryFactory
                 .selectFrom(point)
                 .where(
-                        point.user.id.eq(userId)
+                        point.user.id.eq(userId),
+                        cursor == null ? null :
+                                point.createdAt.lt(cursor.getCreatedAt())
+                                        .or(
+                                                point.createdAt.eq(cursor.getCreatedAt())
+                                                        .and(point.id.lt(cursor.getId()))
+                                        )
                 )
-                .orderBy(point.createdAt.desc())
+                .orderBy(point.createdAt.desc(), point.id.desc())
+                .limit(size + 1)
                 .fetch();
     }
 }
