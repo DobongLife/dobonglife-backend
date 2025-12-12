21b4811971fb3f442c868407af060c8854b27580
Shinjongyun <tigerjong123@gmail.com>
2025-12-12 13:35:49 +0900

    [Feat] #26 BusinessStatusParser

--- src/main/java/com/umust/dobonglife/domain/business/service/BusinessService.java
+++ src/main/java/com/umust/dobonglife/domain/business/service/BusinessService.java
@@ -6,6 +6,7 @@ import com.umust.dobonglife.domain.user.domain.entity.User;
 import com.umust.dobonglife.domain.user.domain.repository.UserRepository;
 import com.umust.dobonglife.global.common.exception.BusinessException;
 import com.umust.dobonglife.global.common.response.ErrorCode;
+import com.umust.dobonglife.global.common.webclient.business.parser.BusinessStatusParser;
 import com.umust.dobonglife.global.common.webclient.service.WebClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class BusinessService {
 
     private final UserRepository userRepository;
     private final WebClientService webClientService;
-    private final BizStatusResponseParser parser;
+    private final BusinessStatusParser parser;
 
     private static final String VALID_CODE = "01";
 
--- src/main/java/com/umust/dobonglife/global/common/webclient/business/dto/response/BusinessStatusResponse.java
+++ src/main/java/com/umust/dobonglife/global/common/webclient/business/dto/response/BusinessStatusResponse.java
@@ -1,7 +1,10 @@
 package com.umust.dobonglife.global.common.webclient.business.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.umust.dobonglife.global.common.webclient.business.data.BusinessData;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
