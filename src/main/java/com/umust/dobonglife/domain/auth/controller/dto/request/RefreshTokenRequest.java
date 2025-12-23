98b7a8701a7d85cd5f542153dc70b75dd1a293bc
Shinjongyun <tigerjong123@gmail.com>
2025-12-23 13:09:10 +0900

    [Feat] #16 AuthController 어노테이션

--- src/main/java/com/umust/dobonglife/domain/auth/controller/dto/request/RefreshTokenRequest.java
+++ src/main/java/com/umust/dobonglife/domain/auth/controller/dto/request/RefreshTokenRequest.java
@@ -1,5 +1,6 @@
 package com.umust.dobonglife.domain.auth.controller.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,5 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshTokenRequest {
+
+    @NotNull(message = "Refresh Token이 없습니다")
     private String refreshToken;
 }
