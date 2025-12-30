76ac62ae04fb7ae4e81ba5cf0b462bd82a0eedb1
gongPyeon <pyeonkang@naver.com>
2025-12-30 20:00:48 +0900

    fix: firebaseConfig 주석처리

--- src/main/java/com/umust/dobonglife/domain/coupon/domain/repository/PromotionRepository.java
+++ src/main/java/com/umust/dobonglife/domain/coupon/domain/repository/PromotionRepository.java
@@ -4,6 +4,11 @@ import com.umust.dobonglife.domain.coupon.domain.entity.Promotion;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+    @Query("SELECT c FROM Promotion c " +
+            "WHERE (:lastId IS NULL OR c.id < :lastId) " +
+            "ORDER BY c.id DESC")
+    Slice<Promotion> findPromotionNoOffset(Long lastId, Pageable pageable);
 }
--- src/main/java/com/umust/dobonglife/global/config/FirebaseConfig.java
+++ src/main/java/com/umust/dobonglife/global/config/FirebaseConfig.java
@@ -17,21 +17,21 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
-    @Value("${firebase.adminsdk.account.path}")
-    private String firebaseAccountPath;
-
-    @PostConstruct
-    public void initialize() {
-        try (InputStream serviceAccount = new ClassPathResource(firebaseAccountPath).getInputStream()) {
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
-        } catch (IOException e) {
-            throw new BusinessException(ErrorCode.SERVER_ERROR_FIREBASE);
-        }
-    }
+//    @Value("${firebase.adminsdk.account.path}")
+//    private String firebaseAccountPath;
+//
+//    @PostConstruct
+//    public void initialize() {
+//        try (InputStream serviceAccount = new ClassPathResource(firebaseAccountPath).getInputStream()) {
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .build();
+//
+//            if (FirebaseApp.getApps().isEmpty()) {
+//                FirebaseApp.initializeApp(options);
+//            }
+//        } catch (IOException e) {
+//            throw new BusinessException(ErrorCode.SERVER_ERROR_FIREBASE);
+//        }
+//    }
 }
