f54ec242554f7c0f0832a9ccd16a4d0125c84cc0
Shinjongyun <tigerjong123@gmail.com>
2026-01-04 01:21:53 +0900

    [Feat] #3 스웨거 어노테이션 추가

--- src/main/java/com/umust/dobonglife/domain/user/controller/dto/request/SignupRequest.java
+++ src/main/java/com/umust/dobonglife/domain/user/controller/dto/request/SignupRequest.java
@@ -1,5 +1,7 @@
 package com.umust.dobonglife.domain.user.controller.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,8 +11,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SignupRequest {
+    @NotNull(message = "이메일은 필수입니다")
+    @Schema(description = "이메일", example = "dobonglife@gmail.com")
     private String email;
+    @NotNull(message = "이름은 필수입니다")
+    @Schema(description = "이름", example = "김도봉")
     private String name;
+    @NotNull(message = "비밀번호는 필수입니다")
+    @Schema(description = "비밀번호", example = "1234")
     private String password;
+    @NotNull(message = "역할은 필수입니다")
+    @Schema(description = "역할", example = "MEMBER")
     private String role;
 }
