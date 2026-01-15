335de0449707a3e94aec8d47aa536c8bae394388
Shinjongyun <tigerjong123@gmail.com>
2026-01-15 10:52:30 +0900

    [Feat] #56 PlaceSummaryResponse에 테마, 좌표 추가

--- src/main/java/com/umust/dobonglife/global/importer/PlaceCsvImporter.java
+++ src/main/java/com/umust/dobonglife/global/importer/PlaceCsvImporter.java
@@ -44,10 +44,6 @@ public class PlaceCsvImporter implements CommandLineRunner {
     @Override
     @Transactional
     public void run(String... args) throws Exception {
-        if (placeRepository.count() > 0) {
-            log.info("[PlaceCsvImporter] place table already has data. import skipped.");
-            return;
-        }
         importPlaces(importPath);
     }
 
