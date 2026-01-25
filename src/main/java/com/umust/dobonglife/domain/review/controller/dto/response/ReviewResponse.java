ce879a34b64eaedcb365498c15ae80e61c0caefd
gongPyeon <pyeonkang@naver.com>
2026-01-25 10:04:48 +0900

    fix: 코스 및 리뷰 등록 시 포인트 반환 필드 추가

--- src/main/java/com/umust/dobonglife/domain/review/controller/dto/response/ReviewResponse.java
+++ src/main/java/com/umust/dobonglife/domain/review/controller/dto/response/ReviewResponse.java
@@ -1,13 +1,17 @@
 package com.umust.dobonglife.domain.review.controller.dto.response;
 import com.umust.dobonglife.domain.review.domain.entity.Review;
 
+import static com.umust.dobonglife.domain.point.domain.vo.PointPolicy.REVIEW_CREATE;
+
 public record ReviewResponse(Long reviewId,
-                             String content) {
+                             String content,
+                             Long point) {
 
     public static ReviewResponse from(Review review) {
         return new ReviewResponse(
                 review.getId(),
-                review.getContent()
+                review.getContent(),
+                REVIEW_CREATE.getPoint()
         );
     }
 }
