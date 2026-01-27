543b669063f9d3b5c10546c7eda3f23880055d3d
gongPyeon <pyeonkang@naver.com>
2026-01-27 11:47:01 +0900

    fix: 알림 타입 변경

--- src/main/java/com/umust/dobonglife/domain/notification/domain/constant/NotificationType.java
+++ src/main/java/com/umust/dobonglife/domain/notification/domain/constant/NotificationType.java
@@ -4,7 +4,7 @@ public enum NotificationType {
     ALL,      // 전체
     UNREAD,   // 읽지 않음
     POINT,    // 포인트 적립/사용 관련 알림
-    EVENT,    // 이벤트 관련 알림
-    REVIEW,   // 후기 관련 알림
-    COURSE    // 코스 관련 알림
+    SCHEDULE,   // 일정 관련 알림
+    COURSE,    // 코스 관련 알림
+    COUPON // 쿠폰 관련 알림
 }
