5a6d12759cd26380f09d409ae8333f3a801817af
gongPyeon <pyeonkang@naver.com>
2026-02-04 15:51:59 +0900

    fix: firebase 테스트 오류 해결 및 자동화 내 firebase 설정 추가

--- src/test/java/com/umust/dobonglife/DobonglifeApplicationTests.java
+++ src/test/java/com/umust/dobonglife/DobonglifeApplicationTests.java
@@ -1,8 +1,10 @@
 package com.umust.dobonglife;
 
+import com.umust.dobonglife.global.external.firebase.FirebaseConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -10,6 +12,9 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 @SpringBootTest
 @ActiveProfiles("test")
 class DobonglifeApplicationTests {
+
+	@MockitoBean
+	private FirebaseConfig firebaseConfig;
 
 	@MockitoBean
 	private JavaMailSender javaMailSender;
