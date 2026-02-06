0c4fab63590dbb5ef3f74f07bd59d462a0bfab23
gongPyeon <pyeonkang@naver.com>
2026-02-06 09:26:03 +0900

    fix: 프리셋 정책 수정에 따른 규칙 변경

--- src/main/java/com/umust/dobonglife/domain/coupon/domain/repository/PresetRepository.java
+++ src/main/java/com/umust/dobonglife/domain/coupon/domain/repository/PresetRepository.java
@@ -1,11 +1,12 @@
 package com.umust.dobonglife.domain.coupon.domain.repository;
 
 import com.umust.dobonglife.domain.preset.domain.Preset;
+import com.umust.dobonglife.global.common.model.constant.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Arrays;
 import java.util.List;
 
 public interface PresetRepository extends JpaRepository<Preset, Long> {
-    List<Preset> findByCategory(String category);
+    List<Preset> findByCategory(Category category);
 }
