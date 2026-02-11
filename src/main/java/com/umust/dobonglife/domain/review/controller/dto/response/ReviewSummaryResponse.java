1da96056fc037d186ea36bbe4a38fb08d2adf0dd
gongPyeon <pyeonkang@naver.com>
2026-02-11 10:20:32 +0900

    fix: 리뷰 반환 오류 해결

--- src/main/java/com/umust/dobonglife/domain/review/controller/dto/response/ReviewSummaryResponse.java
+++ src/main/java/com/umust/dobonglife/domain/review/controller/dto/response/ReviewSummaryResponse.java
@@ -3,6 +3,7 @@ package com.umust.dobonglife.domain.review.controller.dto.response;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.umust.dobonglife.domain.course.controller.dto.response.CourseSummaryResponse;
 import com.umust.dobonglife.domain.review.domain.entity.Review;
+import com.umust.dobonglife.domain.user.domain.entity.User;
 import com.umust.dobonglife.global.common.Identifiable;
 
 import java.time.LocalDateTime;
@@ -17,10 +18,11 @@ public record ReviewSummaryResponse(Long reviewId,
                                           boolean owner
 ) implements Identifiable {
     public static ReviewSummaryResponse from(Long userId, Review review) {
-        boolean owner = userId == review.getUser().getId(); // TODO: 위치 다시 고민
+        User user = review.getUser();
+        boolean owner = (user != null && userId != null) && userId.equals(user.getId());
         return new ReviewSummaryResponse(
                 review.getId(),
-                review.getUser().getName(),
+                (user != null) ? user.getName() : "알 수 없는 사용자",
                 review.getRating(),
                 review.getContent(),
                 review.getImageUrls(),
