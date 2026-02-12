69e840a220972f6eaf803817416a358080259f7e
gongPyeon <pyeonkang@naver.com>
2026-02-12 12:25:27 +0900

    fix: 리뷰 삭제 룰 수정

--- src/main/java/com/umust/dobonglife/domain/review/domain/entity/Review.java
+++ src/main/java/com/umust/dobonglife/domain/review/domain/entity/Review.java
@@ -29,7 +29,7 @@ public class Review {
     private Long placeId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = true)
     private User user;
 
     @Column(name = "rating", nullable = true)
