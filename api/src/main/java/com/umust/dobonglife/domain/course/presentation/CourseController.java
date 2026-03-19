22faa4cbab29ef82b2b8efd4ebaab469b9fb3ad0
gongPyeon <pyeonkang@naver.com>
2026-03-20 01:19:42 +0900

    fix: 코스 import 수정

--- api/src/main/java/com/umust/dobonglife/domain/course/presentation/CourseController.java
+++ api/src/main/java/com/umust/dobonglife/domain/course/presentation/CourseController.java
@@ -9,7 +9,7 @@ import com.umust.dobonglife.domain.course.application.dto.CreateCourseRequest;
 import com.umust.dobonglife.domain.course.application.dto.MyCourseResponse;
 import com.umust.dobonglife.domain.course.application.dto.UpdateCourseRequest;
 import com.umust.dobonglife.domain.place.domain.vo.Theme;
-import com.umust.dobonglife.global.auth.resolver.CurrentUserId;
+import com.umust.dobonglife.global.common.annotation.CurrentUserId;
 import com.umust.dobonglife.global.common.constant.PageSizeType;
 import com.umust.dobonglife.global.common.response.BaseResponse;
 import com.umust.dobonglife.global.common.response.CursorResponse;
