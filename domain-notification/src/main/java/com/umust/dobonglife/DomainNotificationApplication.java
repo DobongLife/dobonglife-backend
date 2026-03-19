6495f3195b14ac55c84660b066ceda49eae3b478
gongPyeon <pyeonkang@naver.com>
2026-03-20 03:44:28 +0900

    refactor: MSA 구조 분리에 따른 build.gralde 재설정

---
+++ domain-notification/src/main/java/com/umust/dobonglife/DomainNotificationApplication.java
@@ -0,0 +1,11 @@
+package com.umust.dobonglife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainNotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DomainNotificationApplication.class, args);
+    }
+}
---
+++ domain-user/src/main/java/com/umust/dobonglife/DomainUserApplication.java
@@ -0,0 +1,11 @@
+package com.umust.dobonglife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DomainUserApplication.class, args);
+    }
+}
