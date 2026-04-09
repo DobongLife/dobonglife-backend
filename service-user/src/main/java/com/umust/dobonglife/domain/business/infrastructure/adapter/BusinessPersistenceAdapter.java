7bfd7bad9089325ac64c1d2dcdf3ef7de894f5f1
Shinjongyun <tigerjong123@gmail.com>
2026-04-09 20:30:19 +0900

    refact: bussiness 도메인도 헥사고날 완료

---
+++ service-user/src/main/java/com/umust/dobonglife/domain/business/infrastructure/adapter/BusinessPersistenceAdapter.java
@@ -0,0 +1,48 @@
+package com.umust.dobonglife.domain.business.infrastructure.adapter;
+
+import com.umust.dobonglife.domain.business.application.port.out.LoadBusinessPort;
+import com.umust.dobonglife.domain.business.application.port.out.SaveBusinessPort;
+import com.umust.dobonglife.domain.business.domain.entity.Business;
+import com.umust.dobonglife.domain.business.infrastructure.jpa.BusinessJpaRepository;
+import com.umust.dobonglife.global.common.constant.Category;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class BusinessPersistenceAdapter implements LoadBusinessPort, SaveBusinessPort {
+
+    private final BusinessJpaRepository businessJpaRepository;
+
+    @Override
+    public boolean existsByUserId(Long userId) {
+        return businessJpaRepository.existsByUserId(userId);
+    }
+
+    @Override
+    public Optional<Business> findByUserId(Long userId) {
+        return businessJpaRepository.findByUserId(userId);
+    }
+
+    @Override
+    public Optional<Category> findCategoryByUserId(Long userId) {
+        return businessJpaRepository.findCategoryByUserId(userId);
+    }
+
+    @Override
+    public Business save(Business business) {
+        return businessJpaRepository.save(business);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        businessJpaRepository.deleteById(id);
+    }
+
+    @Override
+    public void nullifyPlaceById(Long businessId) {
+        businessJpaRepository.nullifyPlaceById(businessId);
+    }
+}
