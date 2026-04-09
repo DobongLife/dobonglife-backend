19ca6520899b0529d0eb1ed0025b7fc42f939c8f
Shinjongyun <tigerjong123@gmail.com>
2026-04-09 20:56:18 +0900

    [Refact] 모듈 이름 변경

--- service-common/src/main/java/com/umust/dobonglife/domain/auth/application/port/in/AuthenticateAccessTokenUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.umust.dobonglife.domain.auth.application.port.in;
-
-import com.umust.dobonglife.domain.auth.application.dto.AuthenticatedUser;
-
-public interface AuthenticateAccessTokenUseCase {
-    AuthenticatedUser authenticate(String accessToken);
-}
